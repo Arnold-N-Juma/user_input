4dc279eafc032b4109191061501d1dd0ffb076cf
Arnold Juma <+>
2024-02-26 22:06:36 +0300

    Finished Creating the user profile

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name=input('What is your name:')
+age=input('Enter your age:')
+location=input('Type your location:')
+print(f"Hello {name},you are {age} years old, and you currently live in {location}")
